ca7e3630dff259c3fe4ac65e3487aba5cf56c69a
BiancaLSO <biancastan90@gmail.com>
2020-05-31 19:44:43 +0200

    Customer validation

--- Nordic_Motor_Home/SPRING/src/main/java/com/example/demo/Repository/CustomerRepository.java
+++ Nordic_Motor_Home/SPRING/src/main/java/com/example/demo/Repository/CustomerRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.Repository;
 
 import com.example.demo.Model.Customer;
+import com.example.demo.Model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -26,7 +27,7 @@ public class CustomerRepository {
     }
 
     public void add(Customer c) {
-        if(!doesZipExist(c.getZipCodeCustomer())) {
+        if(!doesZipCustomerExist(c.getZipCodeCustomer())) {
             String query1 = "INSERT INTO zip (zipCode, city)" +
                     "VALUES (?, ?)";
             template.update(query1, c.getZipCodeCustomer(), c.getCity());
@@ -38,10 +39,15 @@ public class CustomerRepository {
                 c.getDriverSinceDate(), c.getDriverLicenceNumber());
     }
 
-    //@Autowired private JdbcTemplate jdbcTemplate;
-    private boolean doesZipExist(String newZip) { // checks if the zip already exists in the zip table
-        return template.queryForObject("SELECT EXISTS(SELECT zipCode FROM zip " +
+    @Autowired private JdbcTemplate jdbcTemplate;
+    private boolean doesZipCustomerExist(String newZip) { // checks if the zip already exists in the zip table
+        return jdbcTemplate.queryForObject("SELECT EXISTS(SELECT zipCode FROM zip " +
                 "WHERE zipCode = \"" + newZip + "\")", Boolean.class);
+    }
+
+    private boolean doesCityExist(String updatedCity, String zip) {
+        return jdbcTemplate.queryForObject("SELECT EXISTS(SELECT city FROM zip " +
+                "WHERE city = \"" + updatedCity + "\")", Boolean.class);
     }
 
     public Boolean deleteRow(int id) {
@@ -59,17 +65,31 @@ public class CustomerRepository {
     }
 
     public Customer findById(int id) {
-        String query = "SELECT * FROM customers WHERE id = ?";
+        String query = "SELECT c.id, c.firstName, c.lastName, c.address, c.zipCodeCustomer, z.city, c.phoneNumber, " +
+                "c.email, c.driverSinceDate, c.driverLicenceNumber " +
+                "FROM customers c " +
+                "JOIN zip z " +
+                "ON c.zipCodeCustomer = z.zipCode " +
+                "WHERE id = ?";
         RowMapper<Customer> rowMapper = new BeanPropertyRowMapper<>(Customer.class);
-
         return template.queryForObject(query, rowMapper, id);
     }
 
     public void update(Customer c, int id) {
-        String query2 = "UPDATE customers SET firstName = ?, lastName = ?, address = ?, zipCodeCustomer = ?" +
-                " phoneNumber = ?, email = ?, driverLicenceNumber = ?, driverSinceDate = ? WHERE id= ?";
+        //If zip code is new in the db, it has to be stored in the zip table
+        if(!doesZipCustomerExist(c.getZipCodeCustomer())) {
+            String query1 = "INSERT INTO zip (zipCode, city) " +
+                    "VALUES (?, ?)";
+            template.update(query1, c.getZipCodeCustomer(), c.getCity());
+        }
+        if(!doesCityExist(c.getCity(), c.getZipCodeCustomer())) {
+            String query2 = "UPDATE zip SET city = ? WHERE zipCode = ?";;
+            template.update(query2, c.getCity(), c.getZipCodeCustomer());
+        }
 
-        template.update(query2, c.getFirstName(), c.getLastName(), c.getAddress(), c.getZipCodeCustomer(),
+        String query3 = "UPDATE customers SET firstName = ?, lastName = ?, address = ?, zipCodeCustomer = ?" +
+                    " phoneNumber = ?, email = ?, driverLicenceNumber = ?, driverSinceDate = ? WHERE id= ?";
+        template.update(query3, c.getFirstName(), c.getLastName(), c.getAddress(), c.getZipCodeCustomer(),
                 c.getPhoneNumber(), c.getEmail(), c.getDriverLicenceNumber(), c.getDriverSinceDate(), id);
     }
 } 
